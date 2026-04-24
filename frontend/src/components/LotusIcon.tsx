33d3342e30cec4d95f657efa0c544e1459ad13b7
Anish Guruvelli <guruvellianish@gmail.com>
2026-04-24 21:26:17 +0530

    change favicon

--- frontend/src/components/LotusIcon.tsx
+++ frontend/src/components/LotusIcon.tsx
@@ -2,7 +2,7 @@ export function LotusIcon({ size = 24, className = '' }: { size?: number; classN
   return (
     <svg width={size} height={size} viewBox="0 0 56 56" xmlns="http://www.w3.org/2000/svg" className={className}>
       <text x="50%" y="54%" textAnchor="middle" dominantBaseline="middle" fontSize="42">
-        🧘
+        🧘🏻‍♂️
       </text>
     </svg>
   );
